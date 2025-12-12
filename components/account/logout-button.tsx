0787beb2b545e7f9b04228cde1f5babea02459b1
DimitrisPyrkas <dimitrispyrkas@gmail.com>
2025-12-12 13:07:58 +0200

    Fix errors

--- components/account/logout-button.tsx
+++ components/account/logout-button.tsx
@@ -7,7 +7,7 @@ export function LogoutButton() {
   return (
     <Button
       className="w-full rounded-md bg-red-600 text-white py-2 text-sm font-medium hover:bg-red-700"
-      onClick={() => signOut({ callbackUrl: `${window.location.origin}/login` })}
+      onClick={() => signOut({ callbackUrl: '/login' })}
     >
       Logout
     </Button>
